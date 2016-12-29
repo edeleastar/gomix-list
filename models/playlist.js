14b49a18d7003e1dd983f80812495ee65e0dc071
Eamonn de Leastar <edeleastar@wit.ie>
2016-12-29 17:29:09 +0000

    move sample playlist to json file

--- models/playlist.js
+++ models/playlist.js
@@ -1,16 +1,5 @@
 'use strict';
 
-const sample = [
-  {
-    title: 'Opus 9',
-    artist: 'Beethoven',
-  }, {
-    title: 'Opus 23',
-    artist: 'Beethoven',
-  }, {
-    title: 'Opus 33',
-    artist: 'Beethoven',
-  },
-];
+const sample = require('./sample.json');
 
 module.exports = sample;

9428a16e9c808e963cd92de9a6444f9ffc337e2e
Eamonn de Leastar <edeleastar@wit.ie>
2016-12-29 09:30:34 +0000

    install handlebars + rename index to handlebars extension

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 
 app.use(express.static('public'));
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
 
 app.get('/', function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index');
 });
 
 const listener = app.listen(process.env.PORT || 4000, function () {
